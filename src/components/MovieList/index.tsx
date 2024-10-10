8ef2fc7b43fe2bc88aaa7ca68a0af766f06780ba
Carlos Aguilar <carlosaguilarduarte0820@gmail.com>
2024-10-09 21:10:41 -0500

    Add entity detail page

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { MoviesDataType } from "../../interfaces";
-import PaperContainer from "../PaperContainer";
 import { Box, Paper, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import MovieCard from "../MovieCard";
