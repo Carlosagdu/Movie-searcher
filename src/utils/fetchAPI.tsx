87c9e0b75e4ce71c70ee26ca41477ed53ce1725a
Carlos Aguilar <carlosaguilarduarte0820@gmail.com>
2024-10-12 00:30:31 -0500

    Fix issues with throw error

--- src/utils/fetchAPI.tsx
+++ src/utils/fetchAPI.tsx
@@ -2,25 +2,7 @@ const fetchEntityDetails = async (movieURL: string, options: object) => {
   try {
     const res = await fetch(movieURL, options);
 
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const json = await res.json();
-    return json;
-  } catch (err) {
-    console.error("Error fetching movie details:", err);
-    throw err;
-  }
-};
-
-const fetchEntityCredits = async (movieURL: string, options: object) => {
-  try {
-    const res = await fetch(movieURL, options);
-
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const json = await res.json();
+    const json: JSON = await res.json();
     return json;
   } catch (err) {
     console.error("Error fetching movie details:", err);
