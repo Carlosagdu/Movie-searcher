aa89171fdda38dedaadba975ff52d94e766279c9
Carlos Aguilar <carlosaguilarduarte0820@gmail.com>
2024-10-12 00:34:13 -0500

    set json response to any because i don't have the current types from the moviedb api

--- src/utils/fetchAPI.tsx
+++ src/utils/fetchAPI.tsx
@@ -2,7 +2,7 @@ const fetchEntityDetails = async (movieURL: string, options: object) => {
   try {
     const res = await fetch(movieURL, options);
 
-    const json: JSON = await res.json();
+    const json: any = await res.json();
     return json;
   } catch (err) {
     console.error("Error fetching movie details:", err);
